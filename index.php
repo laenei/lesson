eed34de1b6391d8a13538f7734a1f4f33317bd29
Максим А. Трофимов <C:\Users\maxim.BTFOMS\AppData\Roaming\The Bat!\Mail>
2017-10-19 16:05:56 +0800

    lesson2

--- index.php
+++ index.php
@@ -6,29 +6,8 @@
 <body>
 привет
 <?php
-$url=$_GET['url'];
-$url=rtrim($url,'/');
-$url=explode('/',$url);
-require_once 'controllers/'.$url[0].'.php';
-$controller = new $url[0];
-echo $url[1];
-/*$url1=htmlspecialchars($url[1], ENT_QUOTES);
-$controller->$url1();*/
-echo "<br />".$url[0];
-echo "<br />".$url[1];
-echo "<br />".$url[2];
-
-if (isset($url[2])) {
-  $url1=htmlspecialchars($url[1],ENT_QUOTES);
-  $url2=htmlspecialchars($url[2],ENT_QUOTES);
-  $controller->$url1($url2);
-}
-else {
-    if (isset($url[1])) {
-$url1=htmlspecialchars($url[1],ENT_QUOTES);
-$controller->$url1();
-    }
-}
+  require 'libs/Bootstrap.php';
+  $app = new Bootstrap();
 ?>
 </body>
 </html>
