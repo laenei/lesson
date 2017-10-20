958201ae79a75a0258afe93b17028c0131a03330
laenei <C:\Users\maxim.BTFOMS\AppData\Roaming\The Bat!\Mail>
2017-10-20 16:00:05 +0800

    add kavichka

--- libs/controller.php
+++ libs/controller.php
@@ -4,5 +4,16 @@ class Controller {
 		echo "Это главный контроллер";
 		$this->view = new  View();
 	}
+   
+   public function loadModel($name) {
+   $path = 'models/'.$name.'_model.php';
+   
+   if(file_exists($path)) {
+   		require 'models/'.$name.'_model.php';
+   		$modelName = $name.'_Model';
+   		$this->model = new $modelName();
+   }
+
+}
 }
 ?>
