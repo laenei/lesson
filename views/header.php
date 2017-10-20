40fdfe63b118a832ffd5a10a9821f284d380f133
laenei <C:\Users\maxim.BTFOMS\AppData\Roaming\The Bat!\Mail>
2017-10-20 13:55:46 +0800

    navi

--- views/header.php
+++ views/header.php
@@ -4,9 +4,14 @@
   <meta charset="UTF-8">
   <title>Test</title>
   <link rel="stylesheet" href="public/css/default.css">
+  <script src="public/js/jquery.js"></script>
+  <script src="public/js/custom.js"></script>
 </head>
 <body>
 <div id="header">
-  header
+  <br>
+  <a href="index">Index</a>
+  <a href="help">Help</a>
+  <a href="login">Login</a>
 </div>
 <div id="content"></div>
